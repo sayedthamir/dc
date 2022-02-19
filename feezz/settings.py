9c2e6b424782dd2efcd8709838604bad62f9ab4e
thamir <sayedthame@outlook.sa>
2022-02-19 10:59:25 +0530

    sixth commit

--- feezz/settings.py
+++ feezz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8ki1gbv36-wrpi%_x#lzb^yg02p#1jrj-1g#whqr-!s2)47=_r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
